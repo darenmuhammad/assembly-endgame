f9ea79df7009fa94847fa47866e328e06ad0c9d1
darenmuhammad <darenmuhammad10@gmail.com>
2025-07-27 09:35:21 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./App.css";
+import { languages } from "./language.js";
+
+function App() {
+    const [currentWord, setCurrentWord] = useState("react");
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    const languageElements = languages.map((language) => {
+        const styles = {
+            backgroundColor: language.backgroundColor,
+            color: language.color,
+        };
+
+        return (
+            <span key={language.name} className="chip" style={styles}>
+                {language.name}
+            </span>
+        );
+    });
+
+    const letterElements = currentWord
+        .split("")
+        .map((letter, index) => (
+            <span key={index}>{letter.toUpperCase()}</span>
+        ));
+
+    const keyboardElements = alphabet
+        .split("")
+        .map((letter) => <button key={letter}>{letter.toUpperCase()}</button>);
+
+    return (
+        <main>
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>
+                    Guess the word in under 8 attempts to keep the programming
+                    world safe from Assembly!
+                </p>
+            </header>
+            <section className="game-status">
+                <h2>You win!</h2>
+                <p>Well done!</p>
+            </section>
+            <section className="language-chips">{languageElements}</section>
+            <section className="word">{letterElements}</section>
+            <section className="keyboard">{keyboardElements}</section>
+            <button className="new-game">New Game</button>
+        </main>
+    );
+}
+
+export default App;
